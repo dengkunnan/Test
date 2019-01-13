116a9b36dfc5f9c19fc60aaca547f44c64889688
dengkn_home <804612791@qq.com>
2019-01-13 22:49:52 +0800

    add ne status

---
+++ demo/src/main/java/com/example/demo/contoller/HelloContoller.java
@@ -0,0 +1,13 @@
+package com.example.demo.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContoller {
+
+    @RequestMapping("/hello")
+    public  Object testHello() {
+         return "hello springboot";
+    }
+}
