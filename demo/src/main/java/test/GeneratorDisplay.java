9708d804e23adb1d5177f7fe030bd1c8d397416c
dengkn_home <804612791@qq.com>
2019-01-18 01:46:28 +0800

    add Demo

--- demo/src/main/java/test/GeneratorDisplay.java
+++ demo/src/main/java/test/GeneratorDisplay.java
@@ -1,4 +1,4 @@
-package com.example.demo.unit;
+package test;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,7 +16,25 @@ public class GeneratorDisplay {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
+		File configFile = new File("B:\\spring_workspace\\Test\\generatorConfig2.xml");
+/*		configFile.toString();
+		configFile.getPath();
+		FileOutputStream out = null;
+		try {
+			if (!configFile.exists()) {
+				// 先得到文件的上级目录，并创建上级目录，在创建文件
+				configFile.getParentFile().mkdir();
+				configFile.createNewFile();
+			}
+			//创建文件输出流
+			out = new FileOutputStream(configFile);
+			//将字符串转化为字节
+			byte[] byteArr = "FileInputStream Test".getBytes();
+			out.write(byteArr);
+			out.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}*/
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
